53bd5e484ea9b3318eee8078e2929e4dfa86fbde
xxx <xnatthawut1990@gmail.com>
2024-02-08 15:46:16 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Home from './Home';
+
+function App() {
+  return (
+    <>
+    <div class="container-fluid"> 
+     <div className="text-center" >
+      <img src="/llogo.jpg" width={150} alt="logo True" className='m-3' />
+      <div id='imge'>
+        <a href='https://lin.ee/TsQVKql'>   
+      <img src="true1.jpg" alt="nettrue" />
+      <img src="true2.jpg" alt="nettrue" />
+      <img src="true3.jpg" alt="nettrue" /> </a>
+    </div>
+  </div>
+  </div>
+
+
+    <Home/>      
+    </>
+  )
+}
+
+export default App
+
---
+++ src/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Nav.css"
+function Home() {
+  return (
+    <>
+    
+     <footer> 
+      <div class="fixed-bottom" id='box'>
+  <div className="text-center" >
+  
+   <div className="box">
+   <div className="tel"><a href="tel:0928658929" ><img src="โทร_Th-(1).gif" width="100%" alt="" /></a></div>
+    <div className="line"><a href="https://lin.ee/TsQVKql" target='_blank'><img src="line.gif" width="100%" alt="" /></a></div>
+   </div>
+
+  </div>
+  </div>
+  </footer>
+    </>
+  )
+}
+
+export default Home
